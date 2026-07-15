09eef7723fd807134f164aef2ba0b9fda35c6354
tnals <marcelly.r.costa@gmail.com>
2026-07-15 15:45:04 -0300

    comando para o windows não mexer na tela.

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -5,6 +5,8 @@ import dados.*;
 public class Main {
 
 	public static void main(String[] args) {
+		// força o Java 2D a renderizar tudo em escala 1:1 (100%), ignorando a escala do Windows
+        System.setProperty("sun.java2d.uiScale", "1.0");
 		Som.carregarSons();
 		new Janela();
 
